c9222c9ced7465fd574f6618c39607a95f166388
sukumarsure-096 <66790964+sukumarsure-096@users.noreply.github.com>
2021-11-22 16:13:55 +0530

    upload a new file
    Add a new file

---
+++ find larger one in 4 values.py
@@ -0,0 +1,32 @@
+a = int(input('enter a value :'))
+b = int(input('enter b value :'))
+c = int(input('enter c value :'))
+d = int(input('enter d value :'))
+if a>b :
+    if a>c :
+        if a>d:
+            print('a is greater')
+        else:
+            print('d is greater')
+    else:
+        if c>d:
+            print('c is greater')
+        else:
+            print('d is greater')
+else :
+    if b>c:
+        if b>d:
+            print(' b is greater')
+        else:
+            print('d is greater')
+    else:
+        if c>d:
+            print('c is greater')
+        else :
+            print('d is greater')
+            
+
+    
+            
+   
+        
